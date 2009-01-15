7d99d0508dcc8d4a08fe8818025c3e50ec128ebb
Shih-Chuan Kao <sckao@cern.ch>
2009-01-15 16:02:26 +0000

    *** empty log message ***

---
+++ python/ttjetslist_skim.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+
+fileNames = cms.untracked.vstring(
+
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_1.root',
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_2.root',
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_3.root',
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_4.root',
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_5.root',
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_6.root',
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_7.root',
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_8.root',
+'file:/data/top/sckao/SkimMGTtJets/TtJets_PAT1_9.root'
+
+)
+
---
+++ python/ttjetslist_skim1.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+
+fileNames = cms.untracked.vstring(
+
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_1.root',
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_2.root',
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_3.root',
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_4.root',
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_5.root',
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_6.root',
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_7.root',
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_8.root',
+'file:/data/top/sckao/Skim1MGTtJets/TtJets_PATSkim1_9.root'
+
+)
+
