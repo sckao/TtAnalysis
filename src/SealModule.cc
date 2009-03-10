e715049e1f340c573c22af6358d9cb9fce56776f
Shih-Chuan Kao <sckao@cern.ch>
2009-03-10 13:57:33 +0000

    seal plug in for analyzer

---
+++ src/SealModule.cc
@@ -0,0 +1,9 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "TopPhysics/TtAnalysis/src/TtAnalysis.h"
+#include "TopPhysics/TtAnalysis/src/JetAnalysis.h"
+#include "TopPhysics/TtAnalysis/src/MuonAnalysis.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_FWK_MODULE(TtAnalysis);
+DEFINE_FWK_MODULE(JetAnalysis);
+DEFINE_FWK_MODULE(MuonAnalysis);
